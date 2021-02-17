b2cd807fd2ba710de032ec4a62864dc750c1c25d
jhKim988 <fccva3910@gmail.com>
2021-02-17 16:14:09 +0900

    TBC 14

--- TBC/14TBC_Structure/14TBC_Structure.c
+++ TBC/14TBC_Structure/14TBC_Structure.c
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <conio.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <time.h>
 
 #define MAX 41
 #define MAX_TITLE 40
@@ -411,6 +414,153 @@ void read_books2_binary(const char* filename, struct book** books_dptr, int* n)
 	fclose(file);
 }
 */
+
+// 14.16
+/*
+struct personal_owner {
+	char rrn1[7]; // Resident Registration Number
+	char rrn2[8]; // ex: 830422-1185600
+};
+struct company_owner {
+	char crn1[4]; // Company Registration Number
+	char crn2[3]; // ex: 111-22-33333
+	char crn3[6];
+};
+union data{
+	struct personal_owner po;
+	struct company_owner co;
+};
+
+struct car_data{
+	char model[15];
+	int status; // 0 = personal, 1 = company
+	union data ownerinfo;
+};
+
+void print_car(struct car_data car)
+{
+	printf("----------------------------------\n");
+	printf("Car model : %s\n", car.model);
+	if (car.status == 0 ) // personal
+		printf("Personal Owner: %s-%s\n", car.ownerinfo.po.rrn1, car.ownerinfo.po.rrn2);
+	else // company
+		printf("Company Owner: %s-%s-%s\n", car.ownerinfo.co.crn1, car.ownerinfo.co.crn2, car.ownerinfo.co.crn3);
+	printf("----------------------------------\n");
+}
+*/
+
+// 14.17
+/*
+struct personal_owner
+{
+	char rrn1[7]; // Resident Registration Number
+	char rrn2[8]; // ex: 830422-1185600
+};
+struct company_owner
+{
+	char crn1[4]; // Company Registration Number
+	char crn2[3]; // ex: 111-22-33333
+	char crn3[6];
+};
+struct car_data
+{
+	char model[15];
+	int status; // 0 = personal, 1 = company
+	union
+	{
+		struct personal_owner po;
+		struct company_owner co;
+	};
+};
+void print_car(struct car_data car)
+{
+	printf("----------------------------------\n");
+	printf("Car model : %s\n", car.model);
+	if (car.status == 0) // personal
+		printf("Personal Owner: %s-%s\n", car.po.rrn1, car.po.rrn2);
+	else // company
+		printf("Company Owner: %s-%s-%s\n", car.co.crn1, car.co.crn2, car.co.crn3);
+	printf("----------------------------------\n");
+}
+*/
+// 14.20
+/*
+int iamfunction()
+{
+	return 0;
+}
+*/
+
+// 14.21
+/*
+void f1() {
+	return;
+}
+int f2(char i) {
+	return i + 1;
+}
+*/
+
+// 14.22
+/*
+void ToUpper(char* str)
+{
+	while(*str)
+	{
+		*str = toupper(*str);
+		str++;
+	}
+}
+void ToLower(char* str)
+{
+	while(*str)
+	{
+		*str = tolower(*str);
+		str++;
+	}
+}
+void UpdateString(char* str, int(*pf)(int)) // int(__cdecl *pf)(int)
+{
+	while(*str)
+	{
+		*str = (*pf)(*str);
+		str++;
+	}
+}
+*/
+
+// 14.23
+/*
+// K&R Chapter 5.12
+char char3[3] = {'A', 'B', 'C'};
+char(*complicated_function1())[3] // char[3] 포인터를 리턴하는 함수
+{
+	return &char3;
+}
+// 함수 포인터를 선언하려면 이렇게 해야한다...
+char (*(*fptr1)())[3] = complicated_function1;
+
+typedef char(*FRPTC3())[3]; // Function Returns Pointer to Char[3]
+typedef char(*(*PTFRPTC3)())[3];
+
+FRPTC3 *fptr2 = complicated_function1;
+PTFRPTC3 fptr3 = complicated_function1;
+
+typedef char c3[3];
+c3* complicated_function2()
+{
+	return &char3;
+}
+*/
+
+// 14.24
+int temp(int a)
+{
+	return 0;
+}
+int (*g(int a))(int) {
+	return temp;
+}
 
 int main()
 {
@@ -992,58 +1142,418 @@ int main()
 	// 구조체는 int, double, char 각각이 메모리를 차지하고, 중간에 패딩이 들어갈 수 있다.
 	// 그러나 공용체는 int, double, char이 메모리를 같이 사용한다.
 	// 즉 타입이 다른 데이터들이 같은 메모리 공간을 사용한다.
-	union my_union
-	{
-		int i;
-		double d;
-		char c;
-	};
+	// union my_union
+	// {
+	// 	int i;
+	// 	double d;
+	// 	char c;
+	// };
 
-	union my_union uni;						 // 유니온 변수 선언
-	printf("%zd\n", sizeof(union my_union)); // 8, 구조체였다면 int(4) + double(8) + char(1) = 13
-	printf("%lld\n", (long)&uni);
-	printf("%ld %ld %ld\n", (long)&uni.i, (long)&uni.d, (long)&uni.c);
-	// vs 컴파일러에서는 주소가 모두 동일하다. 메모리를 공유하기 때문에..
+	// union my_union uni;						 // 유니온 변수 선언
+	// printf("%zd\n", sizeof(union my_union)); // 8, 구조체였다면 int(4) + double(8) + char(1) = 13
+	// printf("%lld\n", (long)&uni);
+	// printf("%ld %ld %ld\n", (long)&uni.i, (long)&uni.d, (long)&uni.c);
+	// // vs 컴파일러에서는 주소가 모두 동일하다. 메모리를 공유하기 때문에..
 
-	union my_union save[10];
-	printf("%zd\n", sizeof(save)); // 80
+	// union my_union save[10];
+	// printf("%zd\n", sizeof(save)); // 80
 
-	// debug
-	union my_union uni1; // 8바이트, 쓰레기 값으로 CC CC CC CC
-	uni1.c = 'A';		 // 1바이트만 사용 41 CC CC CC
-	printf("%c %x %d\n", uni1.c, (int)uni1.c, uni1.i);
-	// uni1.c = 'A'에서 41(10진법으로 65)로 바뀐다.
-	// 즉, 0xCCCCCC41 = -858993599가 된다. (vscompiler)
+	// // debug
+	// union my_union uni1; // 8바이트, 쓰레기 값으로 CC CC CC CC
+	// uni1.c = 'A';		 // 1바이트만 사용 41 CC CC CC
+	// printf("%c %x %d\n", uni1.c, (int)uni1.c, uni1.i);
+	// // uni1.c = 'A'에서 41(10진법으로 65)로 바뀐다.
+	// // 즉, 0xCCCCCC41 = -858993599가 된다. (vscompiler)
 
-	uni1.i = 0;	  // 00 00 00 00 위에서 1바이트를 사용하는 것을 지우고 4바이트 사용
-	uni1.c = 'A'; // 41 00 00 00
-	// 0x00000041
-	printf("%c %x %d\n", uni1.c, (int)uni1.c, uni1.i); // uni1.i를 정수로 출력해도 65로 제대로 나온다.(00 00 00 00으로 한 번 초기화 했기 때문)
+	// uni1.i = 0;	  // 00 00 00 00 위에서 1바이트를 사용하는 것을 지우고 4바이트 사용
+	// uni1.c = 'A'; // 41 00 00 00
+	// // 0x00000041
+	// printf("%c %x %d\n", uni1.c, (int)uni1.c, uni1.i); // uni1.i를 정수로 출력해도 65로 제대로 나온다.(00 00 00 00으로 한 번 초기화 했기 때문)
 
-	uni1.d = 1.2; // 33 33 33 33 33 33 f3 3f
-	// 0xf33f333333333333
-	printf("%d %f %d %c\n", uni1.i, uni1.d, (int)uni1.c, uni1.c);
-	// uni1.i: 4바이트, 0x 33 33 33 33 = 858993459
-	// uni1.d: 8바이트, 1.200...
-	// uni1.c: 1바이트, 33(10진수로 51), '3'
+	// uni1.d = 1.2; // 33 33 33 33 33 33 f3 3f
+	// // 0xf33f333333333333
+	// printf("%d %f %d %c\n", uni1.i, uni1.d, (int)uni1.c, uni1.c);
+	// // uni1.i: 4바이트, 0x 33 33 33 33 = 858993459
+	// // uni1.d: 8바이트, 1.200...
+	// // uni1.c: 1바이트, 33(10진수로 51), '3'
 
-	union my_union uni2 = uni1; // copy another union
-	union my_union uni3 = {10}; // 문법상(메모리 상) 한 가지 원소로만 초기화한다.
-	union my_union uni4 = {.c = 'A'}; // designated initializer
-	union my_union unio5 = {.d = 1.23, .i = 100}; // DO NOT RECOMMEND
+	// union my_union uni2 = uni1; // copy another union
+	// union my_union uni3 = {10}; // 문법상(메모리 상) 한 가지 원소로만 초기화한다.
+	// union my_union uni4 = {.c = 'A'}; // designated initializer
+	// union my_union unio5 = {.d = 1.23, .i = 100}; // DO NOT RECOMMEND
 
-	uni.i = 123;
-	uni.d = 1.2;
-	uni.c = 'K';
+	// uni.i = 123;
+	// uni.d = 1.2;
+	// uni.c = 'K';
 
-	printf("%d %f %c\n", uni.i, uni.d, uni.c);
+	// printf("%d %f %c\n", uni.i, uni.d, uni.c);
 
-	union my_union* pu = &uni; // pointer to union
-	int x = pu->i;
+	// union my_union* pu = &uni; // pointer to union
+	// int x = pu->i;
 
-	// char를 넣어놓고 사용할 떄는 double로 사용하면...
-	uni.c = 'A';
-	double real = 3.14 * uni.d; // Do Not Recommend
+	// // char를 넣어놓고 사용할 떄는 double로 사용하면...
+	// uni.c = 'A';
+	// double real = 3.14 * uni.d; // Do Not Recommend
 
+	// 14.16 공용체와 구조체를 함께 사용하기
+	// struct car_data my_car = {.model = "Avante", .status = 0, .ownerinfo.po = {"830422", "1185600"}};
+	// struct car_data company_car = {.model = "Sonata", .status = 1, .ownerinfo.co = {"111", "22", "33333"}};
+	// print_car(my_car);
+	// print_car(company_car);
+
+	// 14.17 익명 공용체
+	// struct car_data my_car = {.model = "Avante", .status = 0, .po = {"830422", "1185600"}};
+	// struct car_data company_car = {.model = "Sonata", .status = 1, .co = {"111", "22", "33333"}};
+	// print_car(my_car);
+	// print_car(company_car);
+
+	// struct Vector2D
+	// {
+	// 	union
+	// 	{
+	// 		struct
+	// 		{
+	// 			double x, y;
+	// 		};
+	// 		struct
+	// 		{
+	// 			double i, j;
+	// 		};
+	// 		struct
+	// 		{
+	// 			double arr[2];
+	// 		};
+	// 	};
+	// };
+	// typedef struct Vector2D vec2;
+	// vec2 v = {3.14, 2.99};
+	// printf("%.2f %.2f\n", v.x, v.y);
+	// printf("%.2f %.2f\n", v.i, v.j);
+	// printf("%.2f %.2f\n", v.arr[0], v.arr[1]);
+
+	// // 배열로 쓰면 for문을 통해 순회할 수 있다.
+	// for (int d = 0; d < 2; ++d)
+	// {
+	// 	printf("%.2f \n", v.arr[d]);
+	// }
+	
+	// 14.18 열거형 Enumerated Types
+	// // Motivation
+	// // red: 0, oragne: 1, yellow: 2, green: 3, ...
+	// // if (c == 2) printf("yellow");
+
+	// // define을 이용하여 Symbolic constant를 만들 수 있지만, 컴파일 타임에 오류를 잡을 수 없다.
+
+	// // 열거형을 사용하자. Enumerated type
+	// enum spectrum {red, orange, yellow, green, 	blue, violet};
+	// // red: 0, orange: 1, yellow: 2, green: 3, blue: 4, violet: 5
+	// // enumerator
+
+	// enum spectrum color;
+	// color = blue;
+	
+	// if (color == yellow)
+	// {
+	// 	printf("yellow");
+	// 	// printf(yellow); // 그냥 정수일 뿐이다.
+	// }	
+	// // 정수이기 때문에 for문으로 순회가 가능하다.
+	// for (color = red; color <= violet; color++) // C++에서는 열거형에 대해서 ++를 사용할 수 없다.
+	// {
+	// 	printf("%d\n", color);
+	// }
+
+	// printf("red = %d, orange = %d", red, orange);
+
+	// enum kids {jackjack, dash, snoopy, nano, pitz};
+	// enum kids my_kid = nano;
+	// printf("nano %d %d\n", my_kid, nano);
+
+	// enum levels {low = 100, medium = 500, high = 2000};
+	// int score = 800;
+	// if (score > high)
+	// 	printf("High Score\n");
+	// else if (score > medium)
+	// 	printf("Good jon\n");
+	// else if (score > low)
+	// 	printf("Not bad\n");
+	// else
+	// 	printf("Do your best\n");
+	
+	// enum pet {cat, dog = 10, lion, tiger};
+	// printf("cat %d\n", cat); // 0
+	// printf("lion %d\n", lion); // 11
+	
+	// 14.19 열거형 연습문제
+	// enum spectrum {red, orange, yellow, green, blue};
+	// const char* colors[] = {"red", "orange", "yellow", "green", "blue"};
+
+	// char choice[LEN] = {0, };
+	// enum spectrum color;
+	// bool color_is_found = false;
+
+	// while (1)
+	// {
+	// 	printf("Input a color name (empty line to quit)\n");
+
+	// 	// My Try		
+	// 	// if (fgets(choice, sizeof(choice), stdin) == NULL || choice[0] == '\n')
+	// 	// {
+	// 	// 	break;
+	// 	// }
+	// 	// choice[strlen(choice) - 1] = '\0';
+
+	// 	if (scanf("%[^\n]%*c", choice) != 1)
+	// 	{
+	// 			break;
+	// 	}
+
+	// 	int color;
+	// 	for (color = red; color <= blue; ++color)
+	// 	{
+	// 		if (strcmp(colors[color], choice) == 0)
+	// 			{
+	// 				color_is_found = true;
+	// 				break;
+	// 			}
+	// 	}
+
+	// 	if (color_is_found)
+	// 	{
+	// 		switch (color)
+	// 		{
+	// 			case red:
+	// 			printf("Red Zeppelin\n");
+	// 			break;
+				
+	// 			case orange:
+	// 			printf("Orange juice\n");
+	// 			break;
+
+	// 			case yellow:
+	// 			printf("Yellow submarine\n");
+	// 			break;
+
+	// 			case green:
+	// 			printf("Green green grass of home\n");
+	// 			break;
+
+	// 			case blue:
+	// 			printf("Blue Baby\n");
+	// 			break;
+	// 		}
+	// 		color_is_found = false;
+	// 	} else
+	// 	{
+	// 		printf("Please try different color %s\n", choice);
+	// 	}
+	// }
+	// printf("Good Bye");
+
+	// 14.20 네임 스페이스 (이름공간)
+	// {
+	// 	int my_name = 345;
+	// 	// double my_name; = 3.14; // 같은 스코프에서 이름이 같으면 안된다.
+	// }
+	// struct rect // tag가 rect다.
+	// {
+	// 	double x;
+	// 	double y;
+	// };
+	// // struct rect rect = {1.1, 2.2}; // 구조체 변수 이름을 rect로 할 수 있다. (카테고리가 다르다.)
+	// int rect = 123; // C++에서는 허용되지 않는다.
+
+	// // Not Ok
+	// // typedef struct rect rect;
+	// // rect rect = {1.1, 1.2};
+	
+	// // int iamfunction = iamfunction(); // 충돌
+	
+	// // Namespace pollution(공해)
+	// // 쓸만한 변수 이름을 이미 다른 곳에서 사용하고 있는 경우가 꽤 많다.
+	// // 가급적 네임 스페이스를 작게 사용해야 그런 상황이 적어진다.
+	// // 다른 파일에서 이름이 겹치면 Linking 과정에서 오류가 난다.
+	// // 전역변수를 적게 사용하거나 static 키워드를 사용 하는 등...
+
+	// 14.21 함수 포인터의 원리
+	// void (*pf1)() = f1; // f1 함수가 저장돼 있는 주솟값을 pf1에 저장한다.
+	// // void (*pf1)() = &f1; // & 붙여도 되고 안 붙여도 된다. 함수 이름 자체가 주소(포인터)이기 때문에 &를 안 붙여도 된다.
+
+	// int (*pf2)(char) = f2;
+
+	// (*pf1)(); // call f1 via pf1
+	// //pf1(); // 이렇게 호출해도 된다.
+
+	// int a = pf2('A');
+	// // int a = (*pf2)('A');
+	
+	// // Text Segment(프로그램 코드, Read only)
+	// // 프로그램 작성 -> 컴파일 -> 실행파일 생성(하드디스크에 저장)
+	// // 실행파일 실행 -> 운영체제가 프로그램 자체를 메모리에 올린다. -> 프로그램 코드 자체가 저장되는곳이 Text Segment
+
+	// // 함수 포인터 자체는 주솟값을 저장하는 변수 - stack 메모리에 저장된다.
+	// // 문자열 리터럴, 함수 이름, 메인함수까지도 text segment에 저장된다. 
+	
+	// 14.22 함수포인터 사용방법
+	// char str[] = "Hello, World!";
+	// void (*pf)(char*);
+
+	// pf = ToUpper;
+	// // pf = &ToUpper; // Acceptable
+	// // pf = ToUpper(str); // Not Acceptable in C
+
+	// printf("String Literal %lld\n", (long)("Hello, World!")); // Text Segment
+	// printf("Functional Pointer %lld\n", (long)ToUpper); // Text Segment
+	// printf("Variable %lld\n", (long)str); // Stack
+
+	// (*pf)(str);
+	// printf("ToUpper %s\n", str);
+	// // pf(str); // K&R: No, ANSI: Ok
+
+	// pf = ToLower;
+	// (*pf)(str);
+	// printf("ToLower %s\n", str);
+
+	// UpdateString(str, toupper);
+	// printf("ToUpper %s\n", str);
+
+	// UpdateString(str, tolower);
+	// printf("ToLower %s\n", str);
+
+	// // Node: _cdecl is function calling convention(함수 호출 규약)
+
+	// 14.23 자료형에 별명을 붙여주는 typedef
+	// // Give Symbolic names(or aliases) to types
+	// // Does not create new types
+	// typedef unsigned char BYTE; // typedef [별명을 붙여줄 타입] [별명]
+	// BYTE x, y[10] = {0, }, *z = &x; // 변수, 배열, 포인터 다 된다.
+	// {
+	// 	// typedef도 스코프를 갖는다.
+
+	// 	typedef unsigned char byte;
+
+	// 	// 이식성이 높다.
+	// 	size_t s = sizeof(byte); // x86: sizeof() -> unsigned int, x64: sizeof() -> unsigned long long
+	// 	// unsigned int s = sizeof(byte); // x86
+	// 	// unsigned long long s = sizeof(byte); // x64
+	// 	// x86, x64 각 경우마다 size_t를 따로 정의해둬서 편리하다.
+
+	// }
+	// // byte b; // 스코프 밖이라 사용 불가
+
+	// time_t t = time(NULL); // 1970년 1월 1일 0시 0분 0초부터 몇 초가 지났는지 알려주는 함수
+
+	// printf("%lld\n", t);
+
+	// // typedef vs #define
+	// // 기본적으로는 typedef를 권장한다.
+
+	// typedef char* STRING;
+	// STRING name = "John Wick", sign = "World";
+
+	// // #define은 전처리기, 즉 컴파일하기 전에 처리가 된다. 단순한 복사-붙여넣기이다.
+	// // typedef는 컴파일러가 처리를 해준다.
+
+	// // #define STRING char*
+	// // 전처리기가 STRING을 모두 char*로 바꿔버린다.
+
+	// // 위를 아래로 바꾼다.
+	// // STRING name, sign;
+	// // char* name, sign; // sign은 포인터가 아니게 된다. char* name, *sign;으로 되야하는데...
+
+	// typedef struct complex
+	// {
+	// 	float real;
+	// 	float imag;
+	// } COMPLEXT; // typedef struct complex COMPLEX
+
+	// typedef struct
+	// {
+	// 	double width;
+	// 	double height;
+	// } rect; // No tag
+
+	// rect r1 = {1.1, 2.2};
+	// rect r2 = r1;
+	// printf("%f %f\n", r2.width, r2.height);
+
+	// // K&R Chapter 5.12
+	// char(*ret_val)[3] = fptr1();
+	// printf("%c %c %c\n", (*ret_val)[0], (*ret_val)[1], (*ret_val)[2]);
+
+	// c3 *my_c3 = fptr2();
+	// printf("%c %c %c\n", (*my_c3)[0], (*my_c3)[1], (*my_c3)[2]);
+
+	// 14.24 복잡한 선언을 해석하는 요령
+	// 복잡해지는 이유 - *, (), [] (함수의 매개변수가 들어가는 공간, 배열의 index가 들어가는 공간)
+	// 1. 안쪽에서부터 바깥쪽으로 읽어라.
+	// 2. *보다 (), []가 우선순위가 높다.
+
+	// 식별자(이름)을 먼저 찾는다. (ap)
+	// 우선순위가 높은 []를 확인한다. 즉 ap는 배열이다.
+	// int*을 마지막으로 해석한다. 즉 ap는 int* 10개 배열이다.
+	int* ap[10]; 
+
+	// typedef를 이용해서 생각하면 쉬워진다.
+	typedef int* pint;
+	pint ap[10]; // pint의 10개짜리 배열
+
+	// 1. 이름 찾기 - fp
+	// 2. 우선순위가 높은 ()를 인식 - fp는 함수다. 매개변수로 float가 들어간다.
+	// 3. float*을 리턴하는 함수라는 것을 알 수 있다.
+	float* fp(float);
+
+	// typedef를 이용해서 생각하면
+	typedef float* pfloat;
+	pfloat fp(float); // pfloat 타입을 반환하는 함수
+
+	// 1. 일단 포인터다. *pf
+	// 2. (int)를 보면 함수다. 즉 함수 포인터다.
+	// 3. void를 보면, 아무것도 리턴하지 않는 함수 포인터다.
+	void (*pf)(int);
+
+	// 1. x는 배열이다. []가 *보다 우선순위가 높다.
+	// 2. x는 포인터에 대한 배열이다. *
+	// 3. 이 포인터는 함수에 대한 포인터다. (void)
+	// 4. 이 함수는 int*(정수 포인터)를 리턴하는 함수이다.
+	int* (*x[10])(void);
+
+	// 안되는 것들이 있다.
+	// 1. 함수는 배열을 반환할 수 없다.
+	// int f(int)[];
+	int(*f(int))[]; // 배열에 대한 포인터를 반환할 수는 있다.
+
+	// 2. 함수를 반환할 수는 없다.
+	// int g(int)(int);
+	int(*g(int))(int); // 함수에 대한 포인터를 반환할 수는 있다.
+
+	// 3. 함수의 배열은 불가능하다.
+	// int(x2[10])(int); 
+	int(*x2[10])(int); // 함수 포인터의 배열은 가능하다.
+
+	typedef int FCN(int); // 함수타입
+	typedef FCN* FCN_PTR; // 함수의 포인터
+	typedef FCN_PTR FCN_PTR_ARRAY[10]; // 함수 포인터의 배열
+	FCN_PTR_ARRAY x3;
+
+	// More Example
+	int board[6][4]; // board[6]의 배열
+	int** ptr; // *ptr의 포인터
+
+	int* risks[10]; // 정수 포인터 10개의 배열
+	int(*rusk)[10]; // 정수 배열의 포인터
+
+	int* off[3][4]; // 정수 포인터의 이중배열
+	int(*uff)[3][4]; // 정수의 이중배열의 포인터
+	int(*uof[3])[4]; // (int[4] 배열의 포인터)의 배열
+
+	char* fump(int); // char*를 리턴하는 함수
+	char (*frump)(int); // char를 리턴하는 함수의 포인터
+	char (*flump[3])(int); // 함수 포인터의 배열
+
+	typedef int arr5[5]; // int[5]를 arr5로
+	typedef arr5* p_arr5; // arr5의 포인터를 p_arr5로
+	typedef p_arr5 arrp10[10]; // p_arr5의 10개 배열을 arrp10으로
+	
 	return 0;
 }
