49ed9b8814714714dbc7cb9b33434b06e682b5fb
jhkim988 <fccva3910@gmail.com>
2021-05-31 13:59:12 +0900

    OS Lecture02

--- OperatingSystem/Lecture02/DateServer.java
+++ OperatingSystem/Lecture02/DateServer.java
@@ -1,20 +1,22 @@
 import java.net.*;
 import java.io.*;
 
-public class DateServer {
+public class DateServer { // public class DateServer
     public static void main(String[] args) throws Exception{
-        ServerSocket server = new ServerSocket(6013);
+        ServerSocket server = new ServerSocket(6013); // port number 6013
 
         // Now listen for connections
         while (true) {
-            Socket client = saerver.accept();
+            System.out.println("Listening...");
+            Socket client = server.accept(); // request listening... return client socket(e.g. 7078 port)
             PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
-
+            System.out.println("New Client is Connected.");
             // write the Date to the socket
             pout.println(new java.util.Date().toString());
 
             // close the socket and resume listening for connections
             client.close();
+            System.out.println("Client is Disconnected");
         }
     }
 }
