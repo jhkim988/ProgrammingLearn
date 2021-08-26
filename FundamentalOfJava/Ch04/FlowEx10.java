40fcf81117a1d0695d32ab23bf7a5cae062065f6
jhKim988 <fccva3910@gmail.com>
2021-08-26 16:01:46 +0900

    Java Learn

---
+++ FundamentalOfJava/Ch04/FlowEx10.java
@@ -0,0 +1,25 @@
+public class FlowEx10 {
+  public static void main(String[] args) {
+    int score = 0;
+    char grade = ' ';
+
+    System.out.print("당신의 점수를 입력하세요. (1 ~ 100) > ");
+    Scanner scanner = new Scanner(System.in);
+    score = Integer.parseInt(tmp);
+
+    switch(score / 10) {
+      case 10:
+      case 9:
+        grade = 'A';
+        break;
+      case 8:
+        grade = 'B';
+        break;
+      case 7:
+        grade = 'C';
+        break;
+      default:
+        grade = 'F';
+    }
+  }  
+}
