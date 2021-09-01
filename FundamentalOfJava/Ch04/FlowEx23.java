7ba338a0a76475bf034c4c075ffa5917fb198305
jhKim988 <fccva3910@gmail.com>
2021-09-01 16:22:27 +0900

    Java Learn

---
+++ FundamentalOfJava/Ch04/FlowEx23.java
@@ -0,0 +1,8 @@
+public class FlowEx23 {
+  public static void main(String[] args) {
+    int i = 5;
+    while(i-- != 0) { // --i 와 비교
+      System.out.println(i + " - I can do it.");
+    }
+  } 
+}
