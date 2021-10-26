e21dfd212846db1f1e78a9f680eee3167a7a4a32
jhKim988 <fccva3910@gmail.com>
2021-10-26 15:45:10 +0900

    Java Learn

---
+++ FundamentalOfJava/Ch07/BindingTest2.java
@@ -0,0 +1,18 @@
+public class BindingTest2 {
+  public static void main(String[] args) {
+    Parent p = new Child();
+    Child c = new Child();
+
+    System.out.println("p.x = " + p.x);
+    p.method();
+    System.out.println("c.d = " + c.x);
+    c.method();
+  }  
+}
+class Parent {
+  int x = 100;
+  void method() {
+    System.out.println("Parent Method");
+  }
+}
+class Child extends Parent { }
