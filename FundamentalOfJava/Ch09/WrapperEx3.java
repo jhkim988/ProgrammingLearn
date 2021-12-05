b5bb005a58a77963388f820cd50f6333b80da5de
jhkim988 <fccva3910@gmail.com>
2021-12-05 18:08:34 +0900

    Java Learn

--- FundamentalOfJava/Ch09/WrapperEx3.java
+++ FundamentalOfJava/Ch09/WrapperEx3.java
@@ -13,5 +13,16 @@ public class WrapperEx3 {
     Integer intg2 = new Integer(20);
     int i3 = (int) intg2;
     Integer intg3 = (int) intg2 + i3;
+
+    System.out.println("i = " + i);
+    System.out.println("intg = " + intg);
+    System.out.println("obj = " + obj);
+    System.out.println("lng = " + lng);
+    System.out.println("intg + 10 = " + i2);
+    System.out.println("intg + lng = " + l);
+    System.out.println("intg2 = " + intg2);
+    System.out.println("i3 = " + i3);
+    System.out.println("intg2 + i3 = " + intg3);
+
   }
 }
