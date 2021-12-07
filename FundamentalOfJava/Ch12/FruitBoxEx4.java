55301e625c9cfa20a63e52fa859f5e717c5e8d64
jhKim988 <fccva3910@gmail.com>
2021-12-07 15:56:59 +0900

    Java Learn

---
+++ FundamentalOfJava/Ch12/FruitBoxEx4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Fruit {
+  String name;
+  int weight;
+  Fruit(String name, int weight) {
+    this.name = name;
+    this.weight = weight;
+  }
+  public String toString() { return name + "(" + weight + ")"; }
+}
+
+public class FruitBoxEx4 {
+  
+}
