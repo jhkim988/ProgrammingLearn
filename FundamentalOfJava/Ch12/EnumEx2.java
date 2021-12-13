d54a42a83ebf208a4df91a8e9be816ad2edb8ae1
jhKim988 <fccva3910@gmail.com>
2021-12-13 15:06:45 +0900

    Java learn

--- FundamentalOfJava/Ch12/EnumEx2.java
+++ FundamentalOfJava/Ch12/EnumEx2.java
@@ -13,10 +13,30 @@ enum Direction {
     if (dir < 1 || dir > 4) throw new IllegalArgumentException("Invalid value: " + dir);
     return DIR_ARR[dir - 1];
   }
+  public Direction rotate(int num) {
+    num = num % 4;
+    if (num < 0) num += 4;
+    return DIR_ARR[(value - 1 + num) % 4];
+  }
+  public String toString() {
+    return name() + getSymbol();
+  }
 }
 
 public class EnumEx2 {
   public static void main(String[] args) {
+    for (Direction d : Direction.values()) {
+      System.out.printf("%s=%d\n", d.name(), d.getValue());
+    }
 
+    Direction d1 = Direction.EAST;
+    Direction d2 = Direction.of(1);
+
+    System.out.printf("d1=%s, %d\n", d1.name(), d1.getValue());
+    System.out.printf("d2=%s, %d\n", d2.name(), d2.getValue());
+    System.out.println(Direction.EAST.rotate(1));
+    System.out.println(Direction.EAST.rotate(2));
+    System.out.println(Direction.EAST.rotate(-1));
+    System.out.println(Direction.EAST.rotate(-2));
   }
 }
