e123fa1f34d428893e1f6447b3febd43a4ae287b
jhkim988 <fccva3910@gmail.com>
2022-03-17 16:24:32 +0900

    OS Learn

---
+++ OperatingSystem/Lecture06/Ch06_SharedDB.java
@@ -0,0 +1,47 @@
+package Lecture06;
+
+public class Ch06_SharedDB {
+  private static class SharedDB {
+    private int readCount = 0;
+    private boolean isWriting = false;
+    public void read() {
+  
+    }
+    public void write() {
+  
+    }
+    synchronized public void acquireReadLock() {
+      while (isWriting) {
+        try {
+          wait();
+        } catch (InterruptedException e) { }
+      }
+      readCount++;
+    }
+    synchronized public void releaseReadLock() {
+      readCount--;
+      if (readCount == 0) notify();
+    }
+    synchronized public void acquireWriteLock() {
+      while (readCount > 0 || isWriting) {
+        try {
+          wait();
+        } catch(InterruptedException e) { }
+      }
+      isWriting = true;
+    }
+    synchronized public void releaseWriteLock() {
+      isWriting = false;
+      notifyAll();
+    }
+  }
+  public static void main(String[] args) {
+    SharedDB.acquireReadLock();
+    SharedDB.read();
+    SharedDB.releaseReadLock();
+  
+    SharedDB.acquireWriteLock();
+    SharedDB.write();
+    SharedDB.releaseWriteLock();
+  }
+}
