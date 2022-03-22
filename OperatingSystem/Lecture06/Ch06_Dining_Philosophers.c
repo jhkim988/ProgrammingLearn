bef97e7d2be2c3392cb4b2f3d762ae93d9f66549
jhkim988 <fccva3910@gmail.com>
2022-03-22 16:11:36 +0900

    OS Learn

---
+++ OperatingSystem/Lecture06/Ch06_Dining_Philosophers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define true 1
+#define NUM_PHILS 5
+
+enum {THINKING, HUNGRAY, EATING} state[NUM_PHILS];
+
+pthread_mutex_t mutex_lock;
+pthread_cond_t cond_vars[NUM_PHILS];
+
+int main() {
+  int i;
+  pthread_t tid;
+  init();
+  for (i = 0; i < NUM_PHILS; i++) {
+    pthread_create(&tid, NULL, philosopher, (void*) &i);
+  }
+  for (i = 0; i < NUM_PHILS; i++) {
+    pthread_join(tid, NULL);
+  }
+  return 0;
+}
