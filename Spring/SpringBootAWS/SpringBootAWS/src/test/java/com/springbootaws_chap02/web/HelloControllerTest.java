76d9063f97586935e93e51725362e31676586bd9
jhKim988 <fccva3910@gmail.com>
2022-08-01 16:18:04 +0900

    spring boot Learning(Setting and TestCode)

---
+++ Spring/SpringBootAWS/SpringBootAWS/src/test/java/com/springbootaws_chap02/web/HelloControllerTest.java
@@ -0,0 +1,35 @@
+package com.springbootaws_chap02.web;
+
+import com.springbootaws_chap02.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers= HelloController.class)
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void helloTestReturn_hello() throws Exception {
+        String hello = "hello";
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloResponseDtoTestReturn_helloResponseDto() {
+        String name = "hello";
+        int amount = 1000;
+        mvc.perform(get("/hello/dto").param("name", name).param("amount", String.valueOf(amount)))
+    }
+}
