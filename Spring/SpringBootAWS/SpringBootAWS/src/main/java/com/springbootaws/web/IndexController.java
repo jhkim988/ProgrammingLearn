eed869ab5bc67ec0756dbf5a1dd7b6dc1752ca89
jhKim988 <fccva3910@gmail.com>
2022-08-04 16:08:34 +0900

    SpringBoot Learn

--- Spring/SpringBootAWS/SpringBootAWS/src/main/java/com/springbootaws/web/IndexController.java
+++ Spring/SpringBootAWS/SpringBootAWS/src/main/java/com/springbootaws/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.springbootaws.web;
 
+import com.springbootaws.config.auth.LoginUser;
+import com.springbootaws.config.auth.SessionUser;
 import com.springbootaws.service.posts.PostsService;
 import com.springbootaws.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,13 +11,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -30,6 +38,4 @@ public class IndexController {
         model.addAttribute("post", dto);
         return "posts-update";
     }
-
-
 }
