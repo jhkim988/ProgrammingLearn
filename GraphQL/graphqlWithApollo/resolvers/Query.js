62aa08757ed7d1f8384ff0ad9aedad214d646e65
jhkim988 <fccva3910@gmail.com>
2022-08-08 21:57:32 +0900

    GraphQL Edit

--- GraphQL/graphqlWithApollo/resolvers/Query.js
+++ GraphQL/graphqlWithApollo/resolvers/Query.js
@@ -2,7 +2,7 @@ const Query = {
   totalPhotos: (parent, args, { db }) =>
     db.collection('photos').estimatedDocumentCount(),
   allPhotos: (parent, args, { db }) => 
-    db.collection('photos').find().toArray(),
+    db.collection('photos').find().sort({created: -1}).toArray(),
   totalUsers: (parent, args, { db }) => 
     db.collection('users').estimatedDocumentCount(),
   allUsers: (parent, args, { db }) => 
